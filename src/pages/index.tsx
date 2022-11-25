7a1cc9c271eb70cb42120792b89e8fefd4d3301f
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-11-25 19:52:48 +0000

    Add bot images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { type Component, createSignal, Show, createEffect } from 'solid-js';
 import { Layout } from '../components/shared/Layout';
 import homeGrid from "./../css/home/grid.module.css";
-import utils from "./../css/shared/utils.module.css";
+//import utils from "./../css/shared/utils.module.css";
+import fourBoxGrid from "./../css/shared/4boxGrid.module.css";
 const Index: Component = () => {
   const [randNum, setRandNum] = createSignal(1);
   setRandNum(Math.floor(Math.random() * 2) + 1);
@@ -37,7 +38,36 @@ const Index: Component = () => {
         <div class={homeGrid.div5}>Hi</div>
         <div class={homeGrid.div6}>Hi</div>
         <div class={homeGrid.div7}>Hi</div>
-        <div class={homeGrid.div8}>Hi</div>
+        <section class={homeGrid.div8}>
+          <h3>Our Bots!</h3>
+          <div class={fourBoxGrid.parent}>
+            <div class={fourBoxGrid.div1}>
+              <a href='/bots/starboard'>
+                <img src='https://assets.otterbots.xyz/bot-logos/starboard.png' alt='Starbot' width="150px" height="150px"/>
+              </a>
+            </div>
+            <div class={fourBoxGrid.div2}>
+              <a href='/bots/support'>
+                <img src='https://assets.otterbots.xyz/bot-logos/support.png' alt='Support' width="150px" height="150px"/>
+              </a>
+            </div>
+            <div class={fourBoxGrid.div3}>
+              <a href='/bots/roles'>
+                <img src='https://assets.otterbots.xyz/bot-logos/roles.png' alt='Roles' width="150px" height="150px"/>
+              </a>
+            </div>
+            <div class={fourBoxGrid.div4}>
+              <a href='/bots/stocks'>
+                <img src='https://assets.otterbots.xyz/bot-logos/stocks.png' alt='Stock' width="150px" height="150px"/>
+              </a>
+            </div>
+            <div class={fourBoxGrid.div5}>
+              <a href='/bots/ikeastock'>
+                <img src='https://assets.otterbots.xyz/bot-logos/ikeastock.png' alt='IkeaStock' width="150px" height="150px"/>
+              </a>
+            </div>
+          </div>
+        </section>
         <div class={homeGrid.div9}>Hi</div>
       </div>
     </Layout>
